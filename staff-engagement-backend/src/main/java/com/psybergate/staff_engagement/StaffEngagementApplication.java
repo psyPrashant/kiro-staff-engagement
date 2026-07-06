1ba65329ce6f60195e802e4fdb681054a47abbb1
prashant <prashant.kessa@psybergate.co.za>
2026-07-06 13:36:01 +0200

    Initial commit: Spring Boot backend and Angular frontend scaffolding

---
+++ staff-engagement-backend/src/main/java/com/psybergate/staff_engagement/StaffEngagementApplication.java
@@ -0,0 +1,13 @@
+package com.psybergate.staff_engagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffEngagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffEngagementApplication.class, args);
+	}
+
+}
